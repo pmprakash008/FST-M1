cde6351b8f81029013350622c8dd38108bc8d8db
pmprakash008 <prakash.m11@ibm.com>
2026-06-17 09:36:34 +0530

    Add files via upload

---
+++ SQL/Activities/activity1.sql
@@ -0,0 +1,7 @@
+create table MP(
+    id INT,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+DESCRIBE MP;
---
+++ SQL/Activities/activity2.sql
@@ -0,0 +1,12 @@
+INSERT INTO MP
+VALUES (01, 'Apple', 'chennai');
+
+SELECT * FROM MP;
+
+INSERT ALL
+INTO MP VALUES (02, 'Pit Alex', 'London')
+    INTO MP VALUES(03, 'McLyon', 'Paris')
+    INTO MP VALUES(04, 'Paul Adam', 'Rome')
+    SELECT 1 FROM DUAL;
+
+SELECT * FROM MP;
